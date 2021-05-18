8201393b7c4f99f4ffd06d7966844a8897a9af40
eggei <macitegeercan@gmail.com>
2021-05-18 12:35:49 -0400

    Correct the colors and transitions

--- src/components/MainScene/MainScene.tsx
+++ src/components/MainScene/MainScene.tsx
@@ -5,13 +5,18 @@ const classPrefix = "MainScene MainScene__";
 export const MainScene = () => {
   return (
     <div className="MainSceneWrapper">
-      <div className={classPrefix + "wall"}>
-        <img src="/images/brush-stroke.png" />
-      </div>
+      {/* TABLE START */}
       <div className={classPrefix + "table"}>
         <img src="/images/brush-stroke.png" />
         <div className={classPrefix + "table__frontSurface"} />
       </div>
+      {/* TABLE END */}
+
+      {/* WALL START */}
+      <div className={classPrefix + "wall"}>
+        <img src="/images/brush-stroke.png" />
+      </div>
+      {/* WALL END */}
     </div>
   );
 };
