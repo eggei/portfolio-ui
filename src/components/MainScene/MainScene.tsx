bbb9cb227846fe30d1befa3024861034fffedbb9
eggei <macitegeercan@gmail.com>
2021-05-19 16:56:42 -0400

    Redraw Table and improve the cup drawing

--- src/components/MainScene/MainScene.tsx
+++ src/components/MainScene/MainScene.tsx
@@ -4,31 +4,31 @@ const classPrefix = "MainScene MainScene__";
 
 export const MainScene = () => {
   return (
-    <div className="MainSceneWrapper">
-      {/* Order of the element blocks below is important. For example table elements come before wall elements to be able to select one of the wall elements with css sibling selector (+) from hoveer state of one of the table elements  */}
-
-      {/* TABLE START */}
-      <div className={classPrefix + "table"}>
-        <img src="/images/brush-stroke.png" />
-        <div className={classPrefix + "table__frontSurface"} />
-      </div>
-      {/* TABLE END */}
-
-      {/* WALL START */}
-      <div className={classPrefix + "wall"}>
-        <img src="/images/brush-stroke.png" />
-      </div>
-      {/* WALL END */}
-
-      <div className={classPrefix + "itemsWrapper"}>
-        {/* MUG START */}
-        <div className={classPrefix + "mug"}>
-          <div className={classPrefix + "mug__handle"} />
-          <div className={classPrefix + "mug__top"} />
-          <div className={classPrefix + "mug__bottom"} />
+    <>
+      <img src="/images/blueprint.png" className="blueprint" />
+      <div className="MainSceneWrapper">
+        {/* WALL START */}
+        <div className={classPrefix + "wall"}>
+          <img src="/images/brush-stroke.png" />
         </div>
-        {/* MUG END */}
+        {/* WALL END */}
+
+        {/* TABLE START */}
+        <div className={classPrefix + "table"}>
+          <img src="/images/green-brush-strokes-background.jpeg" />
+        </div>
+        {/* TABLE END */}
+
+        <div className={classPrefix + "itemsWrapper"}>
+          {/* MUG START */}
+          <div className={classPrefix + "mug"}>
+            <div className={classPrefix + "mug__handle"} />
+            <div className={classPrefix + "mug__top"} />
+            <div className={classPrefix + "mug__bottom"} />
+          </div>
+          {/* MUG END */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
