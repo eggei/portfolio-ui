9b440a0f0a41b80be5de4cd84386e17348be3837
eggei <macitegeercan@gmail.com>
2021-05-18 21:26:34 -0400

    Add the mug base

--- src/components/MainScene/MainScene.tsx
+++ src/components/MainScene/MainScene.tsx
@@ -5,6 +5,8 @@ const classPrefix = "MainScene MainScene__";
 export const MainScene = () => {
   return (
     <div className="MainSceneWrapper">
+      {/* Order of the element blocks below is important. For example table elements come before wall elements to be able to select one of the wall elements with css sibling selector (+) from hoveer state of one of the table elements  */}
+
       {/* TABLE START */}
       <div className={classPrefix + "table"}>
         <img src="/images/brush-stroke.png" />
@@ -17,6 +19,16 @@ export const MainScene = () => {
         <img src="/images/brush-stroke.png" />
       </div>
       {/* WALL END */}
+
+      <div className={classPrefix + "itemsWrapper"}>
+        {/* MUG START */}
+        <div className={classPrefix + "mug"}>
+          <div className={classPrefix + "mug__handle"} />
+          <div className={classPrefix + "mug__top"} />
+          <div className={classPrefix + "mug__bottom"} />
+        </div>
+        {/* MUG END */}
+      </div>
     </div>
   );
 };
